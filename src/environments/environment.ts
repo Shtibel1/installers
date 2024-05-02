cfb9e6a50f11604bb3b88f3a0553f0bbe802f496
Nadav <nadavshtibel@gmail.com>
2024-05-02 22:46:07 +0300

    Update environment and styling

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,5 +13,5 @@
 export const environment = {
   production: false,
   // baseApiUrl: "https://8qlxsj92m4.execute-api.eu-north-1.amazonaws.com/Prod/",
-  baseApiUrl: 'https://localhost:7225/',
+  baseApiUrl: 'http://localhost:5085/',
 };
